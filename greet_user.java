56ab998d921163d5817a2c0d4e99615426c08e04
Kaustubh Tripathi (Kyle) <25lbcs1389@culkomail.in>
2025-12-12 00:45:53 +0530

    greet_user.java

--- greet_user.java
+++ greet_user.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class greet_user {
 
     public static void main(String[] args){
+        System.out.println("This program takes your name and greets you, pretty cool, right?");
         Scanner init = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = init.nextLine();
         System.out.println("Hello, " + name + ", have a good day!");
     }
 }
+
