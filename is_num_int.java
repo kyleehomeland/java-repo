57289782c3b24be99d5e551024f22e168c49beca
Kaustubh Tripathi (Kyle) <25lbcs1389@culkomail.in>
2025-12-13 18:32:53 +0530

    Add files via upload
    This program is to check is the given number integer or not

---
+++ is_num_int.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class is_num_int {
+
+    public static void main(String[] args){
+        Scanner checker = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        System.out.println(checker.hasNextInt());
+    }
+}
